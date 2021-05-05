4745029948876916f21df6bb226c75802456a993
Danny JeanLouis <dlouis3386@gmail.com>
2021-05-05 00:56:10 -0400

    completed navbar section with responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,17 @@ const openMenu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close");
 const navigation = document.querySelector(".navbar__navigation");
 
+function closeMobileMenu() {
+  navigation.classList.remove("navbar__navigation--open");
+  openMenu.classList.remove("inactive");
+  closeMenu.classList.add("inactive");
+}
+
 openMenu.addEventListener("click", (evt) => {
   evt.target.classList.add("inactive");
   navigation.classList.add("navbar__navigation--open");
   closeMenu.classList.remove("inactive");
 });
 
-closeMenu.addEventListener("click", (evt) => {
-  evt.target.classList.add("inactive");
-  navigation.classList.remove("navbar__navigation--open");
-  openMenu.classList.remove("inactive");
-});
+closeMenu.addEventListener("click", closeMobileMenu);
+window.addEventListener("resize", closeMobileMenu);
