d1f2f51edf1f730005f06873899baaa4c825b182
Danny JeanLouis <dlouis3386@gmail.com>
2021-05-06 20:35:06 -0400

    completed feature section for tablet 768px

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,20 @@ const openMenu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close");
 const navigation = document.querySelector(".navbar__navigation");
 const navLinks = document.querySelector(".navigation-links");
+const body = document.querySelector("body");
 
 function closeMobileMenu() {
   navigation.classList.remove("navbar__navigation--open");
   openMenu.classList.remove("inactive");
   closeMenu.classList.add("inactive");
+  body.style.overflowY = "initial";
 }
 
 openMenu.addEventListener("click", (evt) => {
   evt.target.classList.add("inactive");
   navigation.classList.add("navbar__navigation--open");
   closeMenu.classList.remove("inactive");
+  body.style.overflowY = "hidden";
 });
 
 closeMenu.addEventListener("click", closeMobileMenu);
